a162b028aca6cda5009c4b562a2aa851008a45db
samruddhitiwari <121943400+samruddhitiwari@users.noreply.github.com>
2025-08-02 22:59:50 +0530

    Delete ingest_to_postgres.py

--- ingest_to_postgres.py
+++
@@ -1,6 +0,0 @@
-import psycopg2
-from sqlalchemy import create_engine
-
-engine = create_engine("postgresql://username:password@localhost:5432/bank")
-customers_df.to_sql("customers", engine, if_exists='replace', index=False)
-transactions_df.to_sql("transactions", engine, if_exists='replace', index=False)
